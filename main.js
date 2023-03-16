325c7f20972fb08f8e5485cd5648c90f5244aacb
blackyselva <selvam17196@gmail.com>
2023-03-16 07:54:42 +0530

    Adding.

---
+++ main.js
@@ -0,0 +1,13 @@
+// 6. Design a method to combine two arrays.
+
+let a = [1,2,3,4,5,6];
+let b = ["Selva","Aravind","Kutti","Kayu"];
+let c = [];
+
+for (i=0;i<a.length;i++) {
+    c.push(a[i]);
+}
+for (j=0;j<b.length;j++) {
+    c.push(b[j]);
+}
+console.log(c);
